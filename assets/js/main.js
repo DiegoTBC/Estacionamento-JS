75dc2a8bab317aa9b47dbc1ff9b07d1cafb98e03
Diego Torres <torressdiiego@outlook.com>
2020-07-29 09:59:06 -0300

    v1

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,82 +3,92 @@ let botaoListarPatio = document.querySelector("#mostrarPatio");
 let botaoFinalizarEdicao = document.querySelector("#finalizarE");
 let botaoDarBaixa = document.querySelector("#darBaixa");
 let botaoPesquisar = document.querySelector("#buscar");
-let inputPesquisar = document.querySelector("#pesquisar"); // ================================ CREATE =======================================
+let inputPesquisar = document.querySelector("#pesquisar");
 
-document.querySelector("#AdicionarCarro").addEventListener('shown.bs.modal', function () {
-  botaoAdicionar.setAttribute("data-dismiss", "modal");
-});
 
-function cadastrarVeiculo(e) {
-  let marcaVeiculo = document.querySelector("#marca").value.toUpperCase();
-  let modeloVeiculo = document.querySelector("#modelo").value.toUpperCase();
-  let placaVeiculo = document.querySelector("#placa").value.toUpperCase();
-  let telefoneDonoVeiculo = document.querySelector("#telefone").value;
-  let tempo = document.querySelector("#tempo").value;
-  let preco = document.querySelector("#preco").value;
-  let horaEntrada = Date.now();
-  let previsaoSaida = horaEntrada + Number(tempo) * 60000;
-  let aux = new Date(horaEntrada);
-  let horaEntradaFormatada = `${aux.getHours()}:${aux.getMinutes()}`;
-  aux = new Date(previsaoSaida);
-  let previsaoSaidaFormatada = `${aux.getHours()}:${aux.getMinutes()}`;
+// ================================ CREATE =======================================
 
-  if (placa.value == "" || telefone.value == "" || tempo.value == "") {
-    botaoAdicionar.removeAttribute("data-dismiss");
-    alert("Você não informou os campos necessário !");
-  } else {
-    let regexPlaca = /\d{3}\s{4}/;
-    let regexTelefone = /\d{9}/;
-    let veiculo = {
-      Marca: marcaVeiculo,
-      Modelo: modeloVeiculo,
-      Placa: placaVeiculo,
-      Telefone: telefoneDonoVeiculo,
-      Preco: preco,
-      Tempo: tempo,
-      HoraEntradaF: horaEntradaFormatada,
-      PrevisaoSaidaFormatada: previsaoSaidaFormatada,
-      HoraEntrada: horaEntrada,
-      PrevisaoSaida: previsaoSaida
-    };
+document.querySelector("#AdicionarCarro").addEventListener('shown.bs.modal', function(){
+    botaoAdicionar.setAttribute("data-dismiss", "modal")
+})
 
-    if (localStorage.getItem('patio') === null) {
-      let veiculos = [];
-      veiculos.push(veiculo);
-      localStorage.setItem('patio', JSON.stringify(veiculos));
+function cadastrarVeiculo(e){  
+    let marcaVeiculo = document.querySelector("#marca").value.toUpperCase()
+    let modeloVeiculo = document.querySelector("#modelo").value.toUpperCase()
+    let placaVeiculo = document.querySelector("#placa").value.toUpperCase()
+    let telefoneDonoVeiculo = document.querySelector("#telefone").value
+    let tempo = document.querySelector("#tempo").value
+    let preco = document.querySelector("#preco").value
+
+    let horaEntrada = Date.now()
+    let previsaoSaida = horaEntrada + (Number(tempo) * 60000)
+
+    let aux = new Date(horaEntrada)
+    let horaEntradaFormatada = `${aux.getHours()}:${aux.getMinutes()}`
+
+    aux = new Date(previsaoSaida)
+    let previsaoSaidaFormatada = `${aux.getHours()}:${aux.getMinutes()}`
+
+    if (placa.value == "" || telefone.value == "" || tempo.value == ""){
+        botaoAdicionar.removeAttribute("data-dismiss")
+        alert("Você não informou os campos necessário !")
     } else {
-      var veiculos = JSON.parse(localStorage.getItem('patio'));
-      veiculos.push(veiculo);
-      localStorage.setItem('patio', JSON.stringify(veiculos));
+        let regexPlaca = /\d{3}\s{4}/
+        let regexTelefone = /\d{9}/
+    
+        let veiculo = {
+            Marca: marcaVeiculo,
+            Modelo: modeloVeiculo,
+            Placa: placaVeiculo,
+            Telefone: telefoneDonoVeiculo,
+            Preco: preco,
+            Tempo: tempo,
+            HoraEntradaF: horaEntradaFormatada,
+            PrevisaoSaidaFormatada: previsaoSaidaFormatada,
+            HoraEntrada: horaEntrada,
+            PrevisaoSaida: previsaoSaida,
+        }
+    
+        if (localStorage.getItem('patio') === null){
+            let veiculos = []
+            veiculos.push(veiculo);
+            localStorage.setItem('patio', JSON.stringify(veiculos))
+        } else {
+            var veiculos = JSON.parse(localStorage.getItem('patio'))
+            veiculos.push(veiculo)
+            localStorage.setItem('patio', JSON.stringify(veiculos))
+        }
+    
+        document.querySelector('form').reset()
+
+        mostrarPatio()
+
     }
+    e.preventDefault()
+};
 
-    document.querySelector('form').reset();
-    mostrarPatio();
-  }
+// ============================== READ ======================================
 
-  e.preventDefault();
-}
+function mostrarPatio(){
+    let veiculos = JSON.parse(localStorage.getItem('patio'))
+    let tabela = document.querySelector(".table")
 
-; // ============================== READ ======================================
-
-function mostrarPatio() {
-  let veiculos = JSON.parse(localStorage.getItem('patio'));
-  let tabela = document.querySelector(".table");
-  let tabelaHTML = `<tr>
+    let tabelaHTML = `<tr>
         <th scope="col">Placa</th>
         <th scope="col">Telefone</th>
         <th scope="col">Tempo</th>
         <th scope="col">Entrada</th>
         <th scope="col">Observações</th>
         <th scope="col">Ações</th>            
-        </tr>`;
+        </tr>`
 
-  if (veiculos.length === 0) {
-    tabela.innerHTML = "";
-  } else {
-    veiculos.forEach(chave => {
-      if (chave.PrevisaoSaida <= Date.now()) {
-        tabelaHTML += `<tr>
+    if (veiculos.length === 0){
+        tabela.innerHTML = ""
+    } else { 
+        veiculos.forEach(chave => {
+            if(chave.PrevisaoSaida <= Date.now()){
+                tabelaHTML +=
+                `<tr>
                 <td scope="row">${chave.Placa}</td>
                 <td>${chave.Telefone}</td>
                 <td>${chave.Tempo}</td>
@@ -98,9 +108,10 @@ function mostrarPatio() {
                         </svg>
                     </button>
                 </td>           
-                </tr> `;
-      } else {
-        tabelaHTML += `<tr>
+                </tr> `
+            } else {
+                tabelaHTML +=
+                `<tr>
                 <td scope="row">${chave.Placa}</td>
                 <td>${chave.Telefone}</td>
                 <td>${chave.Tempo}</td>
@@ -120,30 +131,31 @@ function mostrarPatio() {
                         </svg>
                     </button>
                 </td>          
-                </tr> `;
-      }
-    });
-    tabela.innerHTML = tabelaHTML;
-  }
-}
+                </tr> `
+            }          
+        })  
+        tabela.innerHTML = tabelaHTML
+    }   
+};
 
-;
+function pesquisarVeiculo(placa){
+    let veiculos = JSON.parse(localStorage.getItem('patio'))
+    let tabela = document.querySelector(".table")
 
-function pesquisarVeiculo(placa) {
-  let veiculos = JSON.parse(localStorage.getItem('patio'));
-  let tabela = document.querySelector(".table");
-  let tabelaHTML = `<tr>
+    let tabelaHTML = `<tr>
         <th scope="col">Placa</th>
         <th scope="col">Telefone</th>
         <th scope="col">Tempo</th>
         <th scope="col">Entrada</th>
         <th scope="col">Observações</th>
         <th scope="col">Ações</th>            
-        </tr>`;
-  veiculos.forEach(chave => {
-    if (placa === chave.Placa) {
-      if (chave.PrevisaoSaida <= Date.now()) {
-        tabelaHTML += `<tr>
+        </tr>`
+
+    veiculos.forEach(chave => {
+        if (placa === chave.Placa){ 
+            if(chave.PrevisaoSaida <= Date.now()){
+                tabelaHTML +=
+                `<tr>
                 <td scope="row">${chave.Placa}</td>
                 <td>${chave.Telefone}</td>
                 <td>${chave.Tempo}</td>
@@ -163,9 +175,10 @@ function pesquisarVeiculo(placa) {
                         </svg>
                     </button>
                 </td>           
-                </tr> `;
-      } else {
-        tabelaHTML += `<tr>
+                </tr> `
+            } else {
+                tabelaHTML +=
+                `<tr>
                 <td scope="row">${chave.Placa}</td>
                 <td>${chave.Telefone}</td>
                 <td>${chave.Tempo}</td>
@@ -185,109 +198,111 @@ function pesquisarVeiculo(placa) {
                         </svg>
                     </button>
                 </td>           
-                </tr> `;
-      }
-    }
-  });
-  tabela.innerHTML = tabelaHTML;
-  inputPesquisar.value = "";
-}
-
-; // ================================ UPDATE ======================================
-
-document.querySelector("#editarInfoVeiculo").addEventListener('shown.bs.modal', function () {
-  botaoFinalizarEdicao.setAttribute("data-dismiss", "modal");
-});
-
-function editarInfoVeiculo(placa) {
-  let valorPlaca = placa;
-  let veiculos = JSON.parse(localStorage.getItem('patio'));
-  let body = document.querySelector(".body-edita-carro");
-  let marcaVeiculo = document.querySelector("#marcaE");
-  let modeloVeiculo = document.querySelector("#modeloE");
-  let placaVeiculo = document.querySelector("#placaE");
-  let telefoneDonoVeiculo = document.querySelector("#telefoneE");
-  let tempo = document.querySelector("#tempoE");
-  let preco = document.querySelector("#precoE");
-  veiculos.forEach((valor, indice) => {
-    if (valor.Placa === valorPlaca) {
-      marcaVeiculo.value = valor.Marca;
-      modeloVeiculo.value = valor.Modelo;
-      placaVeiculo.value = valor.Placa;
-      telefoneDonoVeiculo.value = valor.Telefone;
-      tempo.value = valor.Tempo;
-      preco.value = valor.Preco;
-    }
-  });
-  document.querySelector("#tempoE").addEventListener('change', function () {
-    let preco = document.querySelector("#precoE");
-
-    switch (Number(document.querySelector("#tempoE").value)) {
-      case 30:
-        preco.value = "R$ 1,00";
-        break;
-
-      case 60:
-        preco.value = "R$ 1,50";
-        break;
-
-      case 90:
-        preco.value = "R$ 2,00";
-        break;
-
-      case 120:
-        preco.value = "R$ 2,50";
-        break;
-
-      default:
-        preco.value = "";
-        break;
-    }
-  });
-  botaoFinalizarEdicao.addEventListener('click', function () {
-    let veiculos = JSON.parse(localStorage.getItem('patio'));
-
-    if (placaVeiculo.value == "" || telefoneDonoVeiculo.value == "" || tempo.value == "") {
-      alert("Você não informou os campos necessário !");
-    } else {
-      veiculos.forEach(valor => {
-        if (valor.Placa === valorPlaca) {
-          let horaEntrada = valor.HoraEntrada;
-          let previsaoSaida = horaEntrada + Number(tempo.value) * 60000;
-          let aux = new Date(previsaoSaida);
-          let previsaoSaidaFormatada = `${aux.getHours()}:${aux.getMinutes()}`;
-          valor.Marca = marcaVeiculo.value.toUpperCase();
-          valor.Modelo = modeloVeiculo.value.toUpperCase();
-          valor.Placa = placaVeiculo.value.toUpperCase();
-          valor.Telefone = telefoneDonoVeiculo.value;
-          valor.Tempo = tempo.value;
-          valor.Preco = preco.value;
-          valor.PrevisaoSaidaFormatada = previsaoSaidaFormatada;
-          valor.PrevisaoSaida = previsaoSaida;
+                </tr> `
+            } 
         }
-      });
-    }
+    }) 
+    tabela.innerHTML = tabelaHTML 
+    inputPesquisar.value = ""
+};
 
-    localStorage.setItem('patio', JSON.stringify(veiculos));
-    mostrarPatio();
-  });
-}
+// ================================ UPDATE ======================================
 
-; // ======================= DELETE ===================================
+document.querySelector("#editarInfoVeiculo").addEventListener('shown.bs.modal', function(){
+    botaoFinalizarEdicao.setAttribute("data-dismiss", "modal")
+})
 
-document.querySelector("#darBaixaCarro").addEventListener('shown.bs.modal', function () {
-  botaoDarBaixa.setAttribute("data-dismiss", "modal");
-});
+function editarInfoVeiculo(placa){
+    let valorPlaca = placa
+    let veiculos = JSON.parse(localStorage.getItem('patio'))
+    let body = document.querySelector(".body-edita-carro")
 
-function darBaixaVeiculo(placa) {
-  let veiculos = JSON.parse(localStorage.getItem('patio'));
-  let body = document.querySelector(".body-baixa-carro");
-  let botaoFinalizar = document.querySelector(".finalizaBaixa");
-  let valorPlaca = placa;
-  veiculos.forEach((valor, indice) => {
-    if (valor.Placa === placa) {
-      if (valor.PrevisaoSaida <= Date.now()) {
-        body.innerHTML = `
+    let marcaVeiculo = document.querySelector("#marcaE")
+    let modeloVeiculo = document.querySelector("#modeloE")
+    let placaVeiculo = document.querySelector("#placaE")
+    let telefoneDonoVeiculo = document.querySelector("#telefoneE")
+    let tempo = document.querySelector("#tempoE")
+    let preco = document.querySelector("#precoE")
+
+    veiculos.forEach((valor, indice) => {
+        if (valor.Placa === valorPlaca){
+            marcaVeiculo.value = valor.Marca
+            modeloVeiculo.value = valor.Modelo
+            placaVeiculo.value = valor.Placa
+            telefoneDonoVeiculo.value = valor.Telefone
+            tempo.value = valor.Tempo
+            preco.value =  valor.Preco
+        }
+    });
+
+    document.querySelector("#tempoE").addEventListener('change', function(){
+        let preco = document.querySelector("#precoE")
+        switch (Number(document.querySelector("#tempoE").value)) {
+            case 30:
+                preco.value = "R$ 1,00"
+                break;
+            case 60:
+                preco.value = "R$ 1,50"
+                break;
+            case 90:
+                preco.value = "R$ 2,00"
+                break;
+            case 120:
+                preco.value = "R$ 2,50"
+                break
+            default:
+                preco.value = ""
+                break;
+        }
+    })
+
+    botaoFinalizarEdicao.addEventListener('click', function(){
+        let veiculos = JSON.parse(localStorage.getItem('patio'))
+
+        if (placaVeiculo.value == "" || telefoneDonoVeiculo.value == "" || tempo.value == ""){
+            alert("Você não informou os campos necessário !")
+        }else{
+            veiculos.forEach(valor => {
+                if (valor.Placa === valorPlaca){
+                    let horaEntrada = valor.HoraEntrada
+                    let previsaoSaida = horaEntrada + (Number(tempo.value) * 60000)
+                    let aux = new Date(previsaoSaida)
+                    let previsaoSaidaFormatada = `${aux.getHours()}:${aux.getMinutes()}`
+                    valor.Marca = marcaVeiculo.value.toUpperCase()
+                    valor.Modelo = modeloVeiculo.value.toUpperCase()
+                    valor.Placa = placaVeiculo.value.toUpperCase()
+                    valor.Telefone = telefoneDonoVeiculo.value
+                    valor.Tempo = tempo.value
+                    valor.Preco = preco.value
+                    valor.PrevisaoSaidaFormatada = previsaoSaidaFormatada
+                    valor.PrevisaoSaida = previsaoSaida 
+                }
+            })
+    } 
+
+    localStorage.setItem('patio', JSON.stringify(veiculos))
+
+    mostrarPatio()
+    
+    })
+};
+
+// ======================= DELETE ===================================
+
+document.querySelector("#darBaixaCarro").addEventListener('shown.bs.modal', function(){
+    botaoDarBaixa.setAttribute("data-dismiss", "modal")
+})
+
+function darBaixaVeiculo(placa){
+    let veiculos = JSON.parse(localStorage.getItem('patio'))
+    let body = document.querySelector(".body-baixa-carro")
+    let botaoFinalizar = document.querySelector(".finalizaBaixa")
+    let valorPlaca = placa
+
+    veiculos.forEach((valor, indice) => {
+        if (valor.Placa === placa){
+            if (valor.PrevisaoSaida <= Date.now()){
+                body.innerHTML = `
                 <table class="table table-hover">     
                     <tr>
                         <th scope="col">Placa</th>
@@ -305,9 +320,9 @@ function darBaixaVeiculo(placa) {
                     <td>${valor.Preco}</td>
                     <td><span class="badge bg-warning text-dark">Atraso</span></td>
                     <td></td>
-                </table>`;
-      } else {
-        body.innerHTML = `
+                </table>`
+            }else {
+                body.innerHTML = `
                 <table class="table table-hover">     
                     <tr>
                         <th scope="col">Placa</th>
@@ -325,68 +340,63 @@ function darBaixaVeiculo(placa) {
                     <td>${valor.Preco}</td>
                     <td><span class="badge bg-success">No Horário</span></td>
                     <td></td>
-                </table>`;
-      }
-    }
-  }); //Remove veiculo
+                </table>`
+            }
+        }
+    }) 
+    
+    //Remove veiculo
+    botaoDarBaixa.addEventListener('click', function(){
+        let veiculos = JSON.parse(localStorage.getItem('patio'))
 
-  botaoDarBaixa.addEventListener('click', function () {
-    let veiculos = JSON.parse(localStorage.getItem('patio'));
-    veiculos.forEach((valor, indice) => {
-      if (valor.Placa === valorPlaca) {
-        veiculos.splice(indice, 1);
-      }
-    });
-    localStorage.setItem('patio', JSON.stringify(veiculos));
-    mostrarPatio();
-  });
+        veiculos.forEach((valor, indice) => {
+            if (valor.Placa === valorPlaca){
+                veiculos.splice(indice, 1)
+            }
+        })
+    
+        localStorage.setItem('patio', JSON.stringify(veiculos))
+    
+        mostrarPatio()
+    })
 }
 
-;
-/*function confirmarRemocaoVeiculo(placa){
-    let opcao = confirm("Deseja realmente excluir?")
-
-    if(opcao)
-        removerVeiculo(placa)        
-    else
-        return false
-}*/
 // ========================= EVENTS ================================
+
 //* Atualiza o patio a cada 30 segundos e verifica se possui algum veiculo em atraso
-
 setInterval(mostrarPatio, 30000);
+
 botaoAdicionar.addEventListener('click', cadastrarVeiculo);
+
 botaoListarPatio.addEventListener('click', mostrarPatio);
-botaoPesquisar.addEventListener('click', function () {
-  pesquisarVeiculo(inputPesquisar.value.toUpperCase());
+
+botaoPesquisar.addEventListener('click', function(){
+    pesquisarVeiculo(inputPesquisar.value.toUpperCase())    
 });
-inputPesquisar.addEventListener('keypress', function (e) {
-  if (event.keyCode === 13) {
-    pesquisarVeiculo(inputPesquisar.value.toUpperCase());
-  }
+
+inputPesquisar.addEventListener('keypress', function(e){
+    if(event.keyCode === 13){
+        pesquisarVeiculo(inputPesquisar.value.toUpperCase())
+    }   
 });
-document.querySelector("#tempo").addEventListener('change', function () {
-  let preco = document.querySelector("#preco");
 
-  switch (Number(document.querySelector("#tempo").value)) {
-    case 30:
-      preco.value = "R$ 1,00";
-      break;
-
-    case 60:
-      preco.value = "R$ 1,50";
-      break;
-
-    case 90:
-      preco.value = "R$ 2,00";
-      break;
-
-    case 120:
-      preco.value = "R$ 2,50";
-      break;
-
-    default:
-      preco.value = "";
-      break;
-  }
+document.querySelector("#tempo").addEventListener('change', function(){
+    let preco = document.querySelector("#preco")
+    switch (Number(document.querySelector("#tempo").value)) {
+        case 30:
+            preco.value = "R$ 1,00"
+            break;
+        case 60:
+            preco.value = "R$ 1,50"
+            break;
+        case 90:
+            preco.value = "R$ 2,00"
+            break;
+        case 120:
+            preco.value = "R$ 2,50"
+            break
+        default:
+            preco.value = ""
+            break;
+    }
 });
